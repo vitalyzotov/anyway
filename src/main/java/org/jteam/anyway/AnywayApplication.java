97bb3e55090c95c36eb947cc3c0139a530892a8b
vzotov <vitalyzotov@gmail.com>
2022-04-13 08:57:30 +0400

    Create application by Spring Initializr

---
+++ src/main/java/org/jteam/anyway/AnywayApplication.java
@@ -0,0 +1,13 @@
+package org.jteam.anyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnywayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jteam/anyway/AnywayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jteam.anyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnywayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
